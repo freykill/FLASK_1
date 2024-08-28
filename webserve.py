494a03d0ee4499ae9711b35fd7107f6271480237
freykill <freykill1999@gmail.com>
2024-08-28 12:44:36 -0500

    chumbaky

---
+++ webserve.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/unidad1')
+def unidad1():
+    return render_template('unidad1.html')
+
+@app.route('/unidad2')
+def unidad2():
+    return render_template('unidad2.html')
+
+@app.route('/unidad3')
+def unidad3():
+    return render_template('unidad3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
